c8ae4b6058fb8bc5b255062eee0fb5c2422ddcd6
Solidlucho83 <solidlucho@gmail.com>
2021-03-04 12:04:47 -0300

    Cambios Nav Bar

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -11,16 +11,13 @@ const Header = () => {
     <a class="navbar-brand" href="/"> <img src={logo} height="84" width="164"></img> </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
-    </button>
-    
+    </button>    
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav mx-4">      
-      
+      <div class="navbar-nav mx-4">     
         <a class="nav-link active" aria-current="page" href="/videojuegos"> VIDEOJUEGOS</a>
         <a class="nav-link active" aria-current="page" href="/aplicaciones"> APP's </a>
-        <a class="nav-link active" aria-current="page" href="/proyectosweb"> PROYECTOS WEB</a>
-        <a class="nav-link active" aria-current="page"  href="/contacto"> CONTACTO </a>
-        
+        <a class="nav-link disable" aria-current="page" href="#"> PROYECTOS WEB</a>
+        <a class="nav-link disable" aria-current="page"  href="#"> CONTACTO </a>        
       </div>
     </div>
   </div>
