22a24af339ac3b0ae990ba16992597e0590898f4
Solidlucho83 <solidlucho@gmail.com>
2021-03-08 14:07:27 -0300

    Nav Link Animation

--- src/componentes/Web.jsx
+++ src/componentes/Web.jsx
@@ -4,13 +4,9 @@ const Web = ({title, imageSource, descripcion, link } ) => {
     return (
         <div className="card text-center ">              
           <img src={imageSource} alt={title} height="400px"  ></img>
-              <div className="cardbody justify-content-center"><br/>
-              
-         <h4 className="card-title">{title}</h4>
-                 
-                 <p className="card-text mb-2 ">{descripcion}<br />
-</p>
-
+              <div className="cardbody justify-content-center"><br/>              
+         <h4 className="card-title">{title}</h4>                 
+                 <p className="card-text mb-2 ">{descripcion}<br /></p>
               </div>
           </div>
     )
