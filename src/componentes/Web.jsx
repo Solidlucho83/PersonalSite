9a53d461b7055aec4fcf81927689faa361c79731
Solidlucho83 <solidlucho@gmail.com>
2021-03-08 12:21:58 -0300

    proyectos Web v3

--- src/componentes/Web.jsx
+++ src/componentes/Web.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Web = ({title, imageSource, descripcion, link } ) => {
     return (
         <div className="card text-center ">              
-          <img src={imageSource} alt={title} height="300px"  ></img>
+          <img src={imageSource} alt={title} height="400px"  ></img>
               <div className="cardbody justify-content-center"><br/>
               
          <h4 className="card-title">{title}</h4>
